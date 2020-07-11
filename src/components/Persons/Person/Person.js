83d62fb196c8ff00271c508b115fe81ba82ea544
ankush953 <ankushraghuvanshi07@gmail.com>
2020-07-11 18:06:26 +0530

    array instead of single div

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
-import styles from './Person.module.css';
+import React, { Component } from "react";
+import styles from "./Person.module.css";
 
 class Person extends Component {
-    render() {
-        console.log('[Person.js] rendering...');
-        return (
-            <div className={styles.Person}>
-                <p onClick={this.props.clicked}>Hey, I am {this.props.name} and my age is {this.props.age}</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
-        );
-    }
+  render() {
+    return [
+      <p key='i1' onClick={this.props.clicked}>
+        Hey, I am {this.props.name} and my age is {this.props.age}
+      </p>,
+      <p key='i2'>{this.props.children}</p>,
+      <input key='i3'
+        type="text"
+        onChange={this.props.changed}
+        value={this.props.name}
+      ></input>,
+    ];
+  }
 }
 
 export default Person;
