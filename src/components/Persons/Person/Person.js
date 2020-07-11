6f82ccf93c30cde9cfc5673ce681614a0a99b46e
ankush953 <ankushraghuvanshi07@gmail.com>
2020-07-11 18:21:26 +0530

    react.fragment is same as Aux. It is just internally implemented

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import styles from "./Person.module.css";
-import Aux from '../../../hoc/Aux'
+import Aux from "../../../hoc/Aux";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <Aux>
+      <Fragment>
         <p onClick={this.props.clicked}>
           Hey, I am {this.props.name} and my age is {this.props.age}
         </p>
@@ -16,7 +16,7 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         ></input>
-      </Aux>
+      </Fragment>
     );
   }
 }
